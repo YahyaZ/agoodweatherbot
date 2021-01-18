ad1cd110e8a5af45bbde88757dec230cde800320
Yahya Z <yahyaz998@gmail.com>
2021-01-18 22:56:47 +1100

    Add everything else

--- src/app.ts
+++ src/app.ts
@@ -2,30 +2,82 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import TelegramBot from 'node-telegram-bot-api';
+import https from 'https';
 import fetch from 'node-fetch';
 
 const TELEGRAM_API_KEY = process.env.TELEGRAM_API_KEY || '';
 const WEATHER_API_KEY = process.env.OPM_API_KEY || '';
 
-const LOCATION_URL = (city: string): string => `https://nominatim.openstreetmap.org/search?city=${city}&format=json&limit=1`;
+const getLocationUrl = (city: string, country?: string): string => `https://nominatim.openstreetmap.org/search?city=${city}&country=${country}&format=json&limit=1`;
+const getWeatherUrl = (lat: string, lng: string): string => `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&exclude=minutely,hourly,alerts&appid=${WEATHER_API_KEY}`;
 
 const bot = new TelegramBot(TELEGRAM_API_KEY, {polling: true});
 
-const getLocation = async (city: string) => {
-    const response = await fetch(LOCATION_URL(city));
-    const locations: any[] = await response.json();
+const getLocation = async (city: string, country?: string) => {
+    const response = await fetch(getLocationUrl(city, country ?? ''));
+    const location: any[] = await response.json();
 
-    console.log(locations);
-
-    return locations.length === 0 ? {lat: 0, lng: 0} : {lat: locations[0].lat, lng: locations[0].lon};
+    return location.length === 0 ? undefined : {lat: location[0].lat, lng: location[0].lon, place: location[0].display_name};
 }
 
-bot.on('message', (msg) => {
-    console.log(msg, 'hello there');
-});
+const getWeatherData = async (lat: string, lng: string) => {
+    const response = await fetch(getWeatherUrl(lat, lng));
+    const weather = await response.json();
+
+    return {current: weather.current, week: weather.daily};
+}
+
+const getWeekWeatherMessage = (week: any[]) => {
+    return week.reduce((acc, curr) => {
+        console.log(curr);
+        
+        return `${acc}
+
+<b>${new Date(curr.dt).toDateString()}</b>
+${curr.temp.max}/${curr.temp.min}°C
+<i>${curr.weather[0].main}</i>`;
+    }, '');
+}
+
+const sendCityWeatherOptions = async (id: string, city: string, country?: string) => {
+    const location = await getLocation(city, country);
+
+    if (!location) {        
+        return;
+    }
+
+    const {current, week} = await getWeatherData(location.lat, location.lng);
+
+    
+    bot.answerInlineQuery(id, [
+            {
+                type: 'article',
+                id: `${location.place}--Now`,
+                title: 'Weather -- Now',
+                description: `Current weather`,
+                input_message_content: {
+                        message_text: `${location.place}
+${'🌨'} - <b>${current.temp}°C</b>
+
+<i>Feels like ${current.feels_like}°C. ${current.weather[0].description}</i>`,
+                        parse_mode: 'HTML'
+                    }
+            },
+            {
+                type: 'article',
+                id:`${location.place}--Week`,
+                title: 'Weather -- Week',
+                description: 'Weather in the next 7 days',
+                input_message_content: {
+                    message_text: getWeekWeatherMessage(week),
+                    parse_mode: 'HTML',
+                }
+            },
+        ]
+    );
+}
 
 bot.on('inline_query', async ({id, query}) => {
-    console.log(query);
     if (!query) {
         bot.answerInlineQuery(id, [
             {
@@ -51,20 +103,10 @@ bot.on('inline_query', async ({id, query}) => {
         return;
     }
 
-    bot.answerInlineQuery(id, [
-        {
-            type: 'article',
-            id: 'eeoo',
-            title: 'Weather',
-            description: 'Placeholder',
-            input_message_content: {
-                    message_text: 'the test worked'
-                }
-            }
-        ]
-    );
+    await sendCityWeatherOptions(id, query);
 });
 
 process.on('unhandledRejection', (err) => {
+    console.error(err);
     process.exit(1);
 });
